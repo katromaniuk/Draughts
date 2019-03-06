64ecc369a11ea2beb4bf8de734e0c9526743e603
Kat.Romaniuk <c.romaniuk@lancaster.ac.uk>
2019-03-06 23:13:15 +0000

    Added option to check the square coordinates(to test the code), correct results are shown in a dialog window

--- Board.java
+++ Board.java
@@ -8,36 +8,62 @@ public class Board {
 
         JFrame checkers = new JFrame("Draughts");
         JPanel panel = new JPanel();
-        //GridBagLayout bagLayout = new GridBagLayout();
         GridLayout layout = new GridLayout(8,8,0,0);
-        //GridBagConstraints c = new GridBagConstraints();
-        //c.fill = GridBagConstraints.BOTH;
         Square[] s = new Square[64];
+        int returnValueX = 0;
+        int returnValueY = 0;
+        int j = 0;
+        int k = 0;
 
 
         for (int i=0; i<64; i++) {
             if (i >= 0 && i <= 7 || i>= 16 && i<= 23 || i>=32 && i <=39 || i >= 48 && i <= 55) {
                 if (i%2 == 0) {
-                    s[i] = new Square(0, 0, panel, "empty2.png");
+                    s[i] = new Square(k, j, panel, "empty2.png");
+                    k++;
                     panel.setLayout(layout);
+                    if (k > 7) {
+                        k = 0;
+                        j++;
+                    }
                 }
                 else {
-                    s[i] = new Square(0,0, panel, "empty.png");
+                    s[i] = new Square(k, j, panel, "empty.png");
+                    k++;
                     panel.setLayout(layout);
+                    if (k > 7) {
+                        k = 0;
+                        j++;
+                    }
                 }
             }
             else {
                 if (i%2 == 0) {
-                    s[i] = new Square(0, 0, panel, "empty.png");
+                    s[i] = new Square(k, j, panel, "empty.png");
+                    k++;
                     panel.setLayout(layout);
+                    if (k > 7) {
+                        k = 0;
+                        j++;
+                    }
                 }
                 else {
-                    s[i] = new Square(0,0, panel, "empty2.png");
+                    s[i] = new Square(k, j, panel, "empty2.png");
+                    k++;
                     panel.setLayout(layout);
+                    if (k > 7) {
+                        k = 0;
+                        j++;
+                    }
                 }
             }
         }
 
+        int g = 12;
+
+        returnValueX = s[g].getXlocation();
+        returnValueY = s[g].getYlocation();
+        JOptionPane.showMessageDialog(null, "Chosen element: " + g + " --> X location: " + returnValueX + ", Y location: " + returnValueY);
         checkers.setContentPane(panel);
         checkers.setSize(800, 800);
         checkers.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- Square.java
+++ Square.java
@@ -7,26 +7,31 @@ public class Square {
     private JButton b = new JButton(i);
     private int Xlocation;
     private int Ylocation;
-    
+
+
+    //get location on the X axis
     public int getXlocation() { 
-        Xlocation = b.getX();
         return Xlocation;
     }
 
+    //get location on the Y axis
     public int getYlocation() {
-        Ylocation = b.getY();
         return Ylocation;
     }
 
+    //clickable Square constructor, used to fill the board
+    /*Takes up integer to indicate the location on the x axis,
+    *another integer for the location on the y axis,
+    * panel on which the square is to be placed,
+    *string with the name of the file used as a graphical
+    *representation of the square
+    */
     public Square(int x, int y, JPanel p, String s) {
 
         Xlocation = x;
-        b.setAlignmentX(x);
         Ylocation = y;
-        b.setAlignmentY(y);
         i = new ImageIcon(s);
         b.setIcon(i);
-        b.setSize(40, 40);
         p.add(b);
     }
 }
